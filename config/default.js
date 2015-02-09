b85e82f5c6533643e65fcf106348472de6fd992e
Makis Tracend <makis.tracend@gmail.com>
2015-02-08 19:37:58 -0800

    main options

--- config/default.js
+++ config/default.js
@@ -1,6 +1,12 @@
 // Default options
 
 module.exports = {
-
+	host: "", // apptrack host, if serving apptrack from a separate domain
+	domains: [], // list of accepted domains, that can submit tracking data (host is automatically added)
+	routes: {
+		client: "/apptrack.js", // the location where the client-side js is output
+		track: "/track" // the path that is recieving the clinet-side data...
+	},
+	store: "memory"
 
 }
